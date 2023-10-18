243c4255851751920e532751a7c76824c38d3628
Prabin Acharya <acharyaprabin101@gmail.com>
2023-10-18 18:08:57 +0545

    added DrawingManager for drawing shapes

--- src/components/GoogleMaps.tsx
+++ src/components/GoogleMaps.tsx
@@ -1,26 +1,46 @@
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import { useMemo } from "react";
+import {
+  DrawingManager,
+  GoogleMap,
+  Marker,
+  useLoadScript,
+} from "@react-google-maps/api";
+import { useMemo, useRef } from "react";
 import MapActionBar from "./MapActionBar";
 
 const GoogleMaps = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    libraries: ["places", "geometry", "visualization", "drawing"],
   });
   const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
 
-  const handleAddMarker = () => {
-    console.log("Add Marker clicked");
-  };
+  const drawingManagerRef = useRef<google.maps.drawing.DrawingManager | null>(
+    null
+  );
 
-  const handleDrawPath = () => {
-    console.log("Draw Line clicked");
-  };
-
-  const handleDrawPolygon = () => {
-    console.log("Draw Polygon clicked");
-  };
+  // const handleAddMarker = () => {
+  //   console.log("Add Marker clicked");
+  //   if (drawingManagerRef.current) {
+  //     drawingManagerRef.current.setDrawingMode(
+  //       google.maps.drawing.OverlayType.MARKER
+  //     );
+  //   }
+  // };
 
   console.log("MAPS", "########################");
+
+  const onPolygonComplete = (polygon: google.maps.Polygon) => {
+    const path = polygon
+      .getPath()
+      .getArray()
+      .map((coord) => {
+        return {
+          lat: coord.lat(),
+          lng: coord.lng(),
+        };
+      });
+    console.log(path);
+  };
 
   return (
     <div className="h-full w-full">
@@ -32,12 +52,30 @@ const GoogleMaps = () => {
             mapContainerClassName="h-full w-full rounded border"
             center={center}
             zoom={10}
-          />
-          <MapActionBar
+          >
+            <DrawingManager
+              ref={drawingManagerRef as React.RefObject<DrawingManager>}
+              onPolygonComplete={onPolygonComplete}
+              options={{
+                drawingMode: null, // Start without any drawing mode
+                drawingControl: true,
+                drawingControlOptions: {
+                  position: window.google.maps.ControlPosition.TOP_CENTER,
+                  drawingModes: [
+                    window.google.maps.drawing.OverlayType.MARKER,
+                    window.google.maps.drawing.OverlayType.POLYLINE,
+                    window.google.maps.drawing.OverlayType.POLYGON,
+                  ],
+                },
+              }}
+            />
+          </GoogleMap>
+
+          {/* <MapActionBar
             onAddMarker={handleAddMarker}
             onDrawPath={handleDrawPath}
             onDrawPolygon={handleDrawPolygon}
-          />
+          /> */}
         </>
       )}
     </div>
