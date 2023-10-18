44e729c8b2976c893b58c83843d2d4010903deee
Prabin Acharya <acharyaprabin101@gmail.com>
2023-10-18 17:06:17 +0545

    added MapActionBar

--- src/components/GoogleMaps.tsx
+++ src/components/GoogleMaps.tsx
@@ -1,11 +1,24 @@
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
+import MapActionBar from "./MapActionBar";
 
 const GoogleMaps = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
   });
   const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+
+  const handleAddMarker = () => {
+    console.log("Add Marker clicked");
+  };
+
+  const handleDrawPath = () => {
+    console.log("Draw Line clicked");
+  };
+
+  const handleDrawPolygon = () => {
+    console.log("Draw Polygon clicked");
+  };
 
   console.log("MAPS", "########################");
 
@@ -14,12 +27,18 @@ const GoogleMaps = () => {
       {!isLoaded ? (
         <h1>Loading...</h1>
       ) : (
-        <GoogleMap
-          // mapContainerClassName="map-container"
-          mapContainerClassName="h-full w-full rounded border"
-          center={center}
-          zoom={10}
-        />
+        <>
+          <GoogleMap
+            mapContainerClassName="h-full w-full rounded border"
+            center={center}
+            zoom={10}
+          />
+          <MapActionBar
+            onAddMarker={handleAddMarker}
+            onDrawPath={handleDrawPath}
+            onDrawPolygon={handleDrawPolygon}
+          />
+        </>
       )}
     </div>
   );
