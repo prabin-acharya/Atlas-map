e6f63a5629edeba0c499430572b575b0ecc84348
Prabin Acharya <acharyaprabin101@gmail.com>
2023-10-30 14:43:42 +0545

    ActionBar icons

--- src/components/MapActionBar.tsx
+++ src/components/MapActionBar.tsx
@@ -1,4 +1,10 @@
 import React, { Dispatch, SetStateAction } from "react";
+import { BiSolidPencil } from "react-icons/bi";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { PiPolygonFill } from "react-icons/pi";
+import { RiImageFill } from "react-icons/ri";
+import { RxText } from "react-icons/rx";
+import { SlCursor } from "react-icons/sl";
 import { DrawingMode } from "../types";
 
 interface Props {
@@ -27,59 +33,68 @@ const MapActionBar: React.FC<Props> = ({
         className={getButtonStyle(null)}
         onClick={() => setCurrentDrawingMode(null)}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6 text-green-800"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <path d="M12 2L10.59 3.41 18.17 11H2v2h16.17l-7.58 7.59L12 22l10-10-10-10z" />
-        </svg>
+        <SlCursor />
       </button>
       <button
         className={getButtonStyle(DrawingMode.MARKER)}
         onClick={() => setCurrentDrawingMode(DrawingMode.MARKER)}
       >
-        <svg
+        {/* <svg
           xmlns="http://www.w3.org/2000/svg"
           className="w-6 h-6 text-green-800"
           viewBox="0 0 24 24"
           fill="currentColor"
         >
           <path d="M12,2C8.13,2,5,5.13,5,9c0,5.25,7,13,7,13s7-7.75,7-13C19,5.13,15.87,2,12,2z M12,11.5c-1.38,0-2.5-1.12-2.5-2.5 s1.12-2.5,2.5-2.5s2.5,1.12,2.5,2.5S13.38,11.5,12,11.5z" />
-        </svg>
+        </svg> */}
+        <FaMapMarkerAlt />
       </button>
       <button
         className={getButtonStyle(DrawingMode.FREEHAND)}
         onClick={() => setCurrentDrawingMode(DrawingMode.FREEHAND)}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6 text-green-800"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <path d="M3,21c0.4,0,0.8-0.2,1-0.6L20.4,3.6c0.4-0.8,0.1-1.7-0.7-2.1c-0.8-0.4-1.7-0.1-2.1,0.7L1.6,19.1C1.2,19.9,1.5,21,2.3,21H3z" />
-        </svg>
+        <BiSolidPencil />
       </button>
       <button
         className={getButtonStyle(DrawingMode.POLYLINE)}
         onClick={() => setCurrentDrawingMode(DrawingMode.POLYLINE)}
       >
-        PL
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-4 h-4 text-green-800"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+        >
+          <polyline
+            points="2,14 5,6 9,11 14,2"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+          />
+          <circle cx="1" cy="14" r="2" fill="currentColor" />
+          <circle cx="5" cy="6" r="2" fill="currentColor" />
+          <circle cx="9" cy="11" r="2" fill="currentColor" />
+          <circle cx="14" cy="2" r="2" fill="currentColor" />
+        </svg>
       </button>
 
       <button
         className={getButtonStyle(DrawingMode.POLYGON)}
         onClick={() => setCurrentDrawingMode(DrawingMode.POLYGON)}
       >
-        PG
+        <PiPolygonFill />
       </button>
       <button
         className={getButtonStyle(DrawingMode.TEXT)}
         onClick={() => setCurrentDrawingMode(DrawingMode.TEXT)}
       >
-        T
+        <RxText />
+      </button>
+      <button
+        className={getButtonStyle(DrawingMode.IMAGE)}
+        onClick={() => setCurrentDrawingMode(DrawingMode.IMAGE)}
+      >
+        <RiImageFill />
       </button>
     </div>
   );
